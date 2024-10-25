f9021b507581a24604533c66caab289462156cb1
Eduardo Antunes Vicente <97367813+EduardoAVicente@users.noreply.github.com>
2024-10-25 00:00:56 -0300

    correção nos testes

--- Source Code Inspection/src/test/java/TicketMachineTest.java
+++ Source Code Inspection/src/test/java/TicketMachineTest.java
@@ -70,11 +70,7 @@
          Assert.assertEquals(0, saldo);
      }
 
-   @Test
-      public void receberTroco() throws PapelMoedaInvalidaException
-      {
-          Assert.assertNotNull(new TicketMachine(1).getTroco());
-      }
+ 
 
  }
  
