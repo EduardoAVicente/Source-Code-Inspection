8e78d6063f232741b9418c0b48866be56cfe7b90
AnaJessicaSS <ajessicassilva@gmail.com>
2024-10-24 22:14:03 -0300

    Add testes ticketMachine

--- Source Code Inspection/src/test/java/TicketMachineTest.java
+++ Source Code Inspection/src/test/java/TicketMachineTest.java
@@ -3,29 +3,71 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 
-import com.mycompany.ticketmaster.core.TicketMachine;
-import com.mycompany.ticketmaster.exception.SaldoInsuficienteException;
-import junit.framework.Assert;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import org.junit.jupiter.api.Test;
-
-/**
- *
- * @author jehsi
- */
-public class TicketMachineTest {
-    
-    public TicketMachineTest() {
-        
-    }
-    
-    @Test
-    public void ImprimirTicketComSaldoZerado() throws SaldoInsuficienteException 
-    {
-        TicketMachine t = new TicketMachine(3);
-        assertThrows(SaldoInsuficienteException.class, () -> {
-            t.imprimir();
-        });
-    }
-}
+ import com.mycompany.ticketmaster.core.TicketMachine;
+ import com.mycompany.ticketmaster.exception.PapelMoedaInvalidaException;
+ import com.mycompany.ticketmaster.exception.SaldoInsuficienteException;
+ import junit.framework.Assert;
+ import static org.junit.jupiter.api.Assertions.assertThrows;
+ 
+ import org.junit.jupiter.api.Test;
+ 
+ /**
+  *
+  * @author jehsi
+  */
+ public class TicketMachineTest {
+     
+     public TicketMachineTest() {
+         
+     }
+     
+     @Test
+     public void ImprimirTicketComSaldoZerado() throws SaldoInsuficienteException 
+     {
+         TicketMachine t = new TicketMachine(3);
+         assertThrows(SaldoInsuficienteException.class, () -> {
+             t.imprimir();
+         });
+     }
+     
+     @Test
+     public void ImprimirTicketSucesso() throws PapelMoedaInvalidaException, SaldoInsuficienteException 
+     {
+         TicketMachine t = new TicketMachine(2);
+         t.inserir(2);
+         String ticket = t.imprimir();
+         System.out.println(ticket);
+         Assert.assertNotNull(ticket);
+     }
+     
+     @Test
+     public void ImprimirTicketComSaldoAbaixo() throws SaldoInsuficienteException, PapelMoedaInvalidaException 
+     {
+         TicketMachine t = new TicketMachine(5);
+         t.inserir(2);
+         assertThrows(SaldoInsuficienteException.class, () -> {
+             t.imprimir();
+         });
+     }
+     
+     @Test
+     public void InserirDinheiroSucesso() throws PapelMoedaInvalidaException
+     {
+         TicketMachine t = new TicketMachine(5);
+         t.inserir(2);
+         int saldo = t.getSaldo();
+         Assert.assertEquals(2, saldo);
+     }
+     
+     @Test
+     public void InserirDinheiroInvalido() throws PapelMoedaInvalidaException
+     {
+         TicketMachine t = new TicketMachine(5);
+         assertThrows(PapelMoedaInvalidaException.class, () -> {
+             t.inserir(3);
+         });
+         int saldo = t.getSaldo();
+         Assert.assertEquals(0, saldo);
+     }
+ }
+ 
